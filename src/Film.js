a239bc56d46257d84285762f3dd5e957eca1f68c
basanta79 <basanta79@gmail.com>
2019-03-10 08:47:02 +0100

    :construction: Add basic style to Film and mod Gallery

--- src/Film.js
+++ src/Film.js
@@ -1,13 +1,15 @@
 import React from 'react'
 
+import './Film.css'
+
 const IMG_URL = "https://image.tmdb.org/t/p/w200"
 
 const Film = props => {
     // console.log(props)
     return(
-        <div>
-            <img src={IMG_URL + props.details.poster_path} alt=""/>
-            <h1>{props.details.title}</h1>
+        <div className="film">
+            <img className="film__pic" src={IMG_URL + props.details.poster_path} alt=""/>
+            <h1 className="film__name">{props.details.title}</h1>
         </div>
     )
 }
